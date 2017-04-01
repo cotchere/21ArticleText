b3bb56e0ac93100c8d0720a8f2442f45f52aa746
cotchere <colinotchere@192.168.2.108>
2017-04-02 00:29:04 +0200

    commit 4

--- 21-Article/src/controller/AddArticleController.java
+++ 21-Article/src/controller/AddArticleController.java
@@ -1,5 +1,37 @@
 package controller;
 
-public class AddArticleController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextArea;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class AddArticleController implements javafx.fxml.Initializable {
+
+	@FXML
+	private AnchorPane paneTable;
+	
+	@FXML
+	private JFXButton btn_add;
+	
+	@FXML
+	private JFXComboBox<String> cb_brand;
+	
+	@FXML
+	private JFXListView<String> list_article;
+	
+	@FXML
+	private JFXTextArea txt_art;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
